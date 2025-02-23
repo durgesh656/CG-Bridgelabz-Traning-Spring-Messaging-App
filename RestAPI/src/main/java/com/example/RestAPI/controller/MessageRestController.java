507d0b21d969abf89c3252c53dd6ce32a0c46cdc
durgesh656 <durgeshkushwaha656@gmail.com>
2025-02-23 14:09:16 +0530

    [Durgesh kushwaha] Add: new feature 'Use GET Request Method and pass name as query parameter' added

--- RestAPI/src/main/java/com/example/RestAPI/controller/MessageRestController.java
+++ RestAPI/src/main/java/com/example/RestAPI/controller/MessageRestController.java
@@ -1,6 +1,8 @@
 package com.example.RestAPI.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +13,10 @@ public class MessageRestController {
     @RequestMapping(value = {"", "/"})
     public String sayHello(){
         return "Hello from  BridgeLabz.";
+
+    }
+    @GetMapping("/query")
+    public String sayHello(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
